f7c458d3ee747ff7d888fc8ce03c5a67a12cc8df
Uchuvadov Stanislav <inklesspen@yandex.ru>
2025-03-14 17:28:15 +0300

    Update inkgz

--- utils/inkgz.lua
+++ utils/inkgz.lua
@@ -2,8 +2,10 @@ local component = require('component')
 local data = component.data
 local flate_header = string.char(120, 1)
 
-module = {}
+local module = {}
 
+---@param gz string
+---@return string
 function module.decompress(gz)
     -- Not so good GZip header cutter but works for compressed NBT data
     local compressed_data = string.sub(gz, 11, string.len(gz) - 18)
