8b33323ddf6b1a6868f28824bc78da6e2e4984e7
Uchuvadov Stanislav <inklesspen@yandex.ru>
2025-05-20 03:47:41 +0300

    Removed unfinished simple-crop-collector

--- bin/simple-crop-collector.lua
+++
@@ -1,9 +0,0 @@
-local robot = require('robot')
-local component = require('component')
-local inventory = component.inventory_controller
-local geolyzer = component.geolyzer
-
-
-local function saveSeed()
-    
-end
